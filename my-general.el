8838e5094dcefb998bbc180ddad613d6205df1e7
Florian Adamsky <florian@adamsky.it>
2012-08-18 11:31:58 +0200

    Added new function toggle-windows-split() from https://gist.github.com/3383607 and moved all window settings to a special section.

--- my-general.el
+++ my-general.el
@@ -46,10 +46,6 @@
 ;; activate visual-line-mode by default
 (setq line-move-visual nil)
 
-;; window frame title
-(setq frame-title-format "emacs [%b %*%+ %f]")
-(setq icon-title-format "emacs [%b]")
-
 ;; highlight current line
 (global-hl-line-mode 1)
 
@@ -131,6 +127,32 @@
 (require 'color-theme)
 (setq solarized-italic nil)
 (color-theme-solarized-dark)
+
+;;------------------------------------------------------------------------------
+;; Window setup
+;;------------------------------------------------------------------------------
+
+;; window frame title
+(setq frame-title-format "emacs [%b %*%+ %f]")
+(setq icon-title-format "emacs [%b]")
+
+;; Toggle between split windows and a single window (https://gist.github.com/3383607)
+(defun toggle-windows-split()
+  "Switch back and forth between one window and whatever split of
+windows we might have in the frame. The idea is to maximize the
+current buffer, while being able to go back to the previous split
+of windows in the frame simply by calling this command again."
+  (interactive)
+  (if (not (window-minibuffer-p (selected-window)))
+      (progn
+        (if (< 1 (count-windows))
+            (progn
+              (window-configuration-to-register ?u)
+              (delete-other-windows))
+          (jump-to-register ?u))))
+  (my-iswitchb-close))
+
+(define-key global-map (kbd "C-|") 'toggle-windows-split)
 
 ;;------------------------------------------------------------------------------
 ;; Line numbers
