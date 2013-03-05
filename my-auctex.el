13a84fcb3bdbd922e28aa6213b305d3326e9c7b4
Florian Adamsky <florian@adamsky.it>
2013-03-05 19:41:34 +0100

    Customized the folding behavior for macros and environments in AUCTeX

--- my-auctex.el
+++ my-auctex.el
@@ -19,6 +19,26 @@
 (add-hook 'LaTeX-mode-hook (lambda () (TeX-fold-mode t)
                              (TeX-fold-buffer)))
 
+;; customize folding behavior for environments
+(custom-set-variables '(TeX-fold-env-spec-list
+                        '(("[comment]" ("comment"))
+                          ("[figure]" ("figure")))))
+
+;; customize folding behavior for macros
+(custom-set-variables
+ '(TeX-fold-macro-spec-list
+   '(("[f]"     ("footnote" "marginpar"))
+     ("[c]"     ("cite" "parencite"))
+     ("[l]"     ("label"))
+     ("[r]"     ("ref" "pageref" "eqref"))
+     ("[i]"     ("index" "glossary"))
+     ("[1]:||*" ("item"))
+     ("..."     ("dots"))
+     ("(C)"     ("copyright"))
+     ("(R)"     ("textregistered"))
+     ("TM"      ("texttrademark"))
+     (1 ("part" "chapter" "section" "subsection" "subsubsection" "paragraph" "subparagraph" "part*" "chapter*" "section*" "subsection*" "subsubsection*" "paragraph*" "subparagraph*" "emph" "textit" "textsl" "textmd" "textrm" "textsf" "texttt" "textbf" "textsc" "textup" "ac")))))
+
 ;; activate flyspell automatically when entering latex mode
 (add-hook 'LaTeX-mode-hook (lambda () (flyspell-mode)))
 
